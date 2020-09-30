5ce303748f829cfd31756c44b26e3cb7db7adf3e
jammon-unibizcom <71858222+jammon-unibizcom@users.noreply.github.com>
2020-09-30 08:51:15 -0700

    Add files via upload

--- configBackup.sh
+++ configBackup.sh
@@ -6,11 +6,11 @@ rm -rf /home/oracle/ora.7z
 /usr/bin/7za x /home/oracle/F.7z -o/F/R2 -aoa
 rm -rf /home/oracle/F.7z
 mkdir ~/lib
-echo \"ENCRYPTION_WALLET_LOCATION=\" >>$TNS_ADMIN/sqlnet.ora
-echo \" \(SOURCE=\" >>$TNS_ADMIN/sqlnet.ora
-echo \"  \(METHOD=FILE\) \" >>$TNS_ADMIN/sqlnet.ora
-echo \"   \(METHOD_DATA=\" >>$TNS_ADMIN/sqlnet.ora
-echo \"    \(DIRECTORY=/home/oracle/wallet\)\)\) \" >>$TNS_ADMIN/sqlnet.ora
+echo "ENCRYPTION_WALLET_LOCATION=" >>$TNS_ADMIN/sqlnet.ora
+echo " (SOURCE=" >>$TNS_ADMIN/sqlnet.ora
+echo "  (METHOD=FILE) " >>$TNS_ADMIN/sqlnet.ora
+echo "   (METHOD_DATA=" >>$TNS_ADMIN/sqlnet.ora
+echo "    (DIRECTORY=/home/oracle/wallet))) " >>$TNS_ADMIN/sqlnet.ora
 java -jar opc_install.jar -opcId 'oracleidentitycloudservice/RMANbackupservice' -opcPass $3 -container $2 -walletdir ~/wallet -libDir ~/lib -configfile ~/config -host https://swiftobjectstorage.$1.oraclecloud.com/v1/unibiz
 rman target / @/home/oracle/rmanConfig.sql
 sqlplus / as sysdba @/home/oracle/cr_cloudBackup.sql
