ad1212b48278f2d288fd8e0e29599bb577dd2f62
jammon-unibizcom <71858222+jammon-unibizcom@users.noreply.github.com>
2020-09-30 14:02:47 -0700

    Add files via upload

--- setupBackup.sh
+++ setupBackup.sh
@@ -1,10 +1,7 @@
 sudo yum -y install java p7zip
 sudo su oracle sh -c "cd /home/oracle ; wget https://github.com/jammon-unibizcom/rman-backup-scripts/raw/master/configBackup.sh ; chmod +x /home/oracle/configBackup.sh ; /home/oracle/configBackup.sh $1 $2 $3"
 sudo systemctl stop crond.service
-sudo mv /etc/crontab /etc/crontab.bak
-sudo su -c "echo SHELL=/bin/bash >>/etc/crontab"
-sudo su -c "echo PATH=/sbin:/bin:/usr/sbin:/usr/bin >>/etc/crontab"
-sudo su -c "echo MAILTO=root >>/etc/crontab"
+sudo cp /etc/crontab /etc/crontab.bak
 sudo su -c "echo 30 23 \* \* \* oracle /F/R2/r2hotbackup.bat >>/etc/crontab"
 sudo su -c "echo */20 \* \* \* \* oracle /F/R2/rman_cloud_archivelog.bat >>/etc/crontab"
 sudo su -c "echo 00 01 \* \* sun,wed oracle /F/R2/analyze.bat >>/etc/crontab"
